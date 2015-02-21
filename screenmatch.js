5bfd044ffe2dc3c3802b1d6969b7d8418301a62c
bensurman <surman.ben@gmail.com>
2015-02-21 23:04:04 +0000

    added polyfill injector

--- screenmatch.js
+++ screenmatch.js
@@ -2,13 +2,15 @@
     'use strict';
 
     angular
-        .module('angular.screenmatch', [])
+        .module('blocks.screenmatch')
+        .run(polyfillInjector)
         .provider('screenmatchConfig', screenmatchConfig)
         .factory('screenmatch', screenmatch)
         .directive('asmScreen', asmScreen);
 
     screenmatch.$inject = ['$rootScope', '$window', '$interval', '$timeout', 'screenmatchConfig'];
     asmScreen.$inject = ['ngIfDirective', 'screenmatch'];
+    polyfillInjector.$inject = ['$window'];
 
     function screenmatchConfig() {
         /* jshint validthis:true */
@@ -94,7 +96,7 @@
         //             Eg, DOM elements
         //
         //Method: Passes a list of values to is() to compare truthiness.
-        //        If nobind is false (default), also watches for window resize.
+        //        If nobind is false (default), also watches for $window resize.
         //        On resize, evaluates if the truthiness has changed.
         //        Fires a callback if there is a change, returning truthiness.
         //
@@ -206,7 +208,7 @@
             }
         }
 
-        //Exposes the window resize event via broadcast
+        //Exposes the $window resize event via broadcast
         function bindResize() {
             var w = angular.element($window);
             var done = false;
@@ -261,6 +263,65 @@
             };
 
             ngIf.link.apply(ngIf, arguments);
+        }
+    }
+
+
+    function polyfillInjector($window) {
+
+        var needsPolyfill = angular.isUndefined($window.matchMedia) || !angular.isFunction($window.matchMedia('all').addListener);
+
+        if (needsPolyfill) {
+            $window.matchMedia = (function () {
+            /*! matchMedia() polyfill - Test a CSS media type/query in JS.
+            * Authors & copyright (c) 2012: Scott Jehl, Paul Irish, Nicholas Zakas, David Knight.
+            * Dual MIT/BSD license
+            **/
+
+                // For browsers that support matchMedium api such as IE 9 and webkit
+                var styleMedia = ($window.styleMedia || $window.media);
+
+                // For those that don't support matchMedium
+                if (!styleMedia) {
+                    var style = document.createElement('style'),
+                    script = document.getElementsByTagName('script')[0],
+                    info = null;
+
+                    style.type = 'text/css';
+                    style.id = 'matchmediajs-test';
+
+                    script.parentNode.insertBefore(style, script);
+
+                    // 'style.currentStyle' is used by IE <= 8
+                    // '$window.getComputedStyle' for all other browsers
+                    info = ('getComputedStyle' in $window) && $window.getComputedStyle(style, null) || style.currentStyle;
+
+                    styleMedia = {
+                        matchMedium: function(media) {
+                            var text = '@media ' + media + '{ #matchmediajs-test { width: 1px; } }';
+
+                            // 'style.styleSheet' is used by IE <= 8
+                            // 'style.textContent' for all other browsers
+                            if (style.styleSheet) {
+                                style.styleSheet.cssText = text;
+                            } else {
+                                style.textContent = text;
+                            }
+
+                            // Test if media query is true or false
+                            return info.width === '1px';
+                        }
+                    };
+                }
+
+                return function(media) {
+                    return {
+                        matches: styleMedia.matchMedium(media || 'all'),
+                        media: media || 'all'
+                    };
+                };
+
+            })();
         }
     }
 })();
