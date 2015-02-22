501e4634ea248e451fe8aca5ed914d9e9aae1ee1
bensurman <surman.ben@gmail.com>
2015-02-22 15:14:06 +0000

    minified

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,14 @@ var gulp = require('gulp');
 var plug = require('gulp-load-plugins')();
 
 var source = [
-    './src/*.js'
+    './src/angular-screenmatch.js'
 ];
 
-gulp.task('createPackage', function(){
+gulp.task('makeDist', function(){
     return gulp
         .src(source)
         .pipe(plug.ngAnnotate({add: true, single_quotes: true}))
-        .pipe(plug.concat('angular-screenmatch.min.js'))
         .pipe(plug.uglify())
+        .pipe(plug.rename('angular-screenmatch.min.js'))
         .pipe(gulp.dest('./dist'));
 });
